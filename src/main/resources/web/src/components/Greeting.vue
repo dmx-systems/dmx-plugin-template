d13126c7ad736c7a320253a553cca2ad7e52860f
Jörg Richter <jri@deepamehta.de>
2018-03-15 00:09:45 +0100

    Bump version number to 0.2-SNAPSHOT

--- src/main/resources/web/src/components/Greeting.vue
+++ src/main/resources/web/src/components/Greeting.vue
@@ -1,12 +1,19 @@
 <template>
-  <button>{{text}}</button>
+  <el-button @click="click">{{text}}</el-button>
 </template>
 
 <script>
 export default {
+
   data () {
     return {
       text: 'Hello DeepaMehta!'
+    }
+  },
+
+  methods: {
+    click () {
+      console.log('Button clicked')
     }
   }
 }
