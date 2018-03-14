a795cb64c40d8d6e1ef0249f3078f445b18b2888
Jörg Richter <jri@deepamehta.de>
2018-03-15 00:52:53 +0100

    Adapt imports to `vue-loader 13` and later

--- src/main/resources/web/src/main.js
+++ src/main/resources/web/src/main.js
@@ -2,13 +2,13 @@ export default {
 
   storeModule: {
     name: 'greeting',
-    module: require('./greeting')
+    module: require('./greeting').default
   },
 
   components: [
     {
       extensionPoint: 'dm5.webclient.toolbar',
-      component: require('./components/Greeting')
+      component: require('./components/Greeting').default
     }
   ]
 }
