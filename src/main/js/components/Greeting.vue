a10d9864debd5f8399f5e852140ebda041e585f4
Jörg Richter <jri@deepamehta.de>
2018-10-01 16:57:06 +0200

    Component illustrates `dm5`, `axios`, `Vue` injection

--- src/main/js/components/Greeting.vue
+++ src/main/js/components/Greeting.vue
@@ -5,6 +5,22 @@
 <script>
 export default {
 
+  inject: {
+    dm5:  'dm5',
+    http: 'axios',
+    Vue:  'Vue'
+  },
+
+  created () {
+    console.log('Greeting created!', this.dm5, this.http, this.Vue)
+    this.http.get('/core/topic/0').then(response => {
+      console.log(new this.dm5.Topic(response.data))
+    })
+    this.Vue.nextTick(() => {
+      console.log('Hello Vue!')
+    })
+  },
+
   computed: {
     buttonLabel () {
       return this.$store.state.greeting.buttonLabel
